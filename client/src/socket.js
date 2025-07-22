3c09a79f7b47a5c6473c10bfb7668c424948e948
Mike Ross <mrosstech@gmail.com>
2025-07-22 15:57:52 -0400

    updated reference to socket_url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4001';
+// Use environment variable for socket URL, fallback to localhost for development
+const URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:4001';
 console.log("socket called");
 export const socket = io(URL, {
     autoConnect: false
